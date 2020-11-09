19e61964b285b9bf2011b1f704b192cdec869ba8
aleksandra-tarasenko <tarasenko.sashulya@gmail.com>
2020-11-09 13:34:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Triplet{
+public:
+    int x;
+    int y;
+    int z;
+    Triplet(int x1, int y1, int z1){
+        x = x1;
+        y = y1;
+        z = z1;
+    }
+    Triplet operator +(Triplet a){
+        
+    }
+
+};
